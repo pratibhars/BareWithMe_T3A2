6844877a11be7b6e02bdca1d41b793769bba527d
Pratibha <68182689+pratibhars@users.noreply.github.com>
2021-02-09 21:28:13 +1000

    initial commit

---
+++ backend/data/products.js
@@ -0,0 +1,81 @@
+const products = [
+  {
+    _id: "1",
+    name: "Face Mask Set",
+    image: "/images/facemask.jpg",
+    description:
+      "face mask set, leaves your skin shining and feeling super moisturised!",
+    brand: "Master Lab",
+    category: "Skincare",
+    price: 35.5,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Cleanser",
+    image: "/images/cleanser.jpg",
+    description:
+      "Removes dirt, makeup, pollutants from your skin. Leaves you feeling Fresh and Squeaky clean",
+    brand: "Laniege",
+    category: "Skincare",
+    price: 25.0,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Orchid Enriched Moisturiser",
+    image: "/images/moisturiser.jpg",
+    description:
+      "Formulated with Jeju orchid extracts that penetrates deep to improve skin within ",
+    brand: "Innisfree",
+    category: "Skincare",
+    price: 30.0,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Jade Roller",
+    image: "/images/jaderoller.jpg",
+    description:
+      "Roll away all the stress, great way to circulate serums and moisturisers and massage the skin",
+    brand: "Salt by Hendrix",
+    category: "Skincare",
+    price: 59.0,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "Aloe Vera Lipstick/Lip Balm",
+    image: "/images/lipbalm.jpg",
+    description:
+      "99% Aloe Vera soothing gel lipbalm, lips are left feeling baby soft",
+    brand: "Peixen",
+    category: "Skincare",
+    price: 15.95,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "Sunscreen",
+    image: "/images/sunscreen.jpg",
+    description: "Daily UV Defense, SPF 36+ Sunscreen",
+    brand: "Innisfree",
+    category: "Skincare",
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+module.exports = products
